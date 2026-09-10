21e85a3714016cb93c7b265674bcee4b70a7c381
polina-romashkina <romaskapola7@gmail.com>
2026-09-10 23:13:39 +0500

    Task1

---
+++ Tyuiu.RomashkinaPB.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.RomashkinaPB.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Полина";
+            int age = 17;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey(); 
+
+
+        }
+    }
+}
