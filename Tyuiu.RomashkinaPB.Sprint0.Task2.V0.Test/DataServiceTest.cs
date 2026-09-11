b6c559a592c39e9aa9a1117eba9d337b94684f6c
polina-romashkina <romaskapola7@gmail.com>
2026-09-11 21:18:01 +0500

    Task2

---
+++ Tyuiu.RomashkinaPB.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.RomashkinaPB.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RomashkinaPB.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки 
+            var name = "Полина";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Полина", res);
+
+        }
+    }
+}
---
+++ Tyuiu.RomashkinaPB.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.RomashkinaPB.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RomashkinaPB.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса dataService и метода GetMessage
+            //из библиотеки Tyuiu.RomashkinaPB.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Полина"));
+            Console.ReadKey();
+        }
+    }
+}
