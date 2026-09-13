07d31038686df944f8e1a772afca814cc7ca38d2
polina-romashkina <romaskapola7@gmail.com>
2026-09-13 17:18:52 +0500

    Task6

---
+++ Tyuiu.RomashkinaPB.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.RomashkinaPB.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.RomashkinaPB.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5, };
+
+            //Пример циклической структуры находится в библиотеке классов в методе AddtionArray
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+            //Пример циклической структуры находится в библиотеке классов в методе SubtractionArray
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+
+            //Пример циклической структуры находится в библиотеке классов в методе AddtionArray
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
